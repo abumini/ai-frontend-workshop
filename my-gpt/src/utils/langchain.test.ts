117acb7d2dad6f11caada37096f739c5f2ec0dc4
Filipe Sousa <36072629+abumini@users.noreply.github.com>
2024-06-05 11:54:24 +0000

    added chat history and context to the llm

--- my-gpt/src/utils/langchain.test.ts
+++ my-gpt/src/utils/langchain.test.ts
@@ -3,13 +3,19 @@ import { generateAnswer } from "./langchain";
 
 describe("LangChain", () => {
   it("Answers a question", async () => {
-    const answer = await generateAnswer("Is the United Kingdom a country?");
+    const answer = await generateAnswer(
+      "Is the United Kingdom a country?",
+      'Take the role of a encyclopedia, that answers questions in a strict way with "yes" or "no" only.',
+    );
 
     assert.equal(answer.trim(), "Yes");
   });
 
   it("Answers a question incorrectly", async () => {
-    const answer = await generateAnswer("Is Amsterdam a city?");
-    assert.notEqual(answer.trim(), "No.");
+    const answer = await generateAnswer(
+      "Is Amsterdam a city?",
+      'Take the role of a encyclopedia, that answers questions in a strict way with "yes" or "no" only.',
+    );
+    assert.notEqual(answer.trim(), "No");
   });
 });
